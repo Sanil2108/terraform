f2b31a20772cb8f8a19ffc2ef154908cacf074a6
Sanil <sanilkhurana7@gmail.com>
2021-01-10 15:12:07 +0530

    Adding first example

---
+++ example.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2.70"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "ap-south-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0b44050b2d893d5f7"
+  instance_type = "t2.micro"
+}
